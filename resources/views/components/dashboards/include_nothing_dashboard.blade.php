e79b1a24d642eb66136ecfa5b28fe5192c2b92e0
valmirjuniorr <valmir_junyor@hotmail.com>
2018-06-29 22:49:34 -0300

    ajuste na exibição da logo nas dashboards vazias

--- resources/views/components/dashboards/include_nothing_dashboard.blade.php
+++ resources/views/components/dashboards/include_nothing_dashboard.blade.php
@@ -1,3 +1,3 @@
-<div class="text-center" style="margin-top: 21%; padding-bottom: 21%;">
-    <img src="{{asset('img/teeth.png')}}" class="rounded mx-auto d-block">
+<div class="text-center" style="margin-top: 21%; padding-bottom: 21%; opacity: 0.4">
+    <img style="width: 164px; height: 164px;" src="{{asset('img/teeth.png')}}" class="rounded mx-auto d-block">
 </div>
