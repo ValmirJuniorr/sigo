f913d0df711051b7c645ff5b0382aba43743126f
valmirjuniorr <valmir_junyor@hotmail.com>
2018-07-26 21:37:02 -0300

    Crud on Group Questions Done

--- app/Http/Controllers/GroupQuestionController.php
+++ app/Http/Controllers/GroupQuestionController.php
@@ -23,12 +23,50 @@ class GroupQuestionController extends Controller
         try{
             $this->groupQuestion->title = $request->input('name');
             $this->groupQuestion->procedure_id = $request->input('procedure_id');
-            //$this->groupQuestion->priority = $this->groupQuestion->get_last_priority($this->groupQuestion->procedure_id);
-            $this->groupQuestion->priority = 1;
+            $this->groupQuestion->priority = $this->groupQuestion->get_last_priority($this->groupQuestion->procedure_id);
             $this->groupQuestion->create($this->groupQuestion);
             return back();
         }catch (ValidationException $e){
-            return null;
+            return back()->withErrors($e->getMessage());
+        }
+    }
+
+    public function edit(Request $request)
+    {
+        try{
+            $this->groupQuestion->id = $request->input('id');
+            $this->groupQuestion->title = $request->input('name');
+            $this->groupQuestion->edit($this->groupQuestion);
+            return back();
+        }catch (ValidationException $e){
+            return back()->withErrors($e->getMessage());
+        }
+    }
+
+    public function change_prioriry(Request $request)
+    {
+        $group_question_one = new GroupQuestion();
+        $group_question_two = new GroupQuestion();
+        try{
+            $group_question_one->id = $request->input('group_question_id_one');
+            $group_question_two->id = $request->input('group_question_id_two');
+            GroupQuestion::change_priority($group_question_one,$group_question_two);
+            return back();
+        }catch (ValidationException $e){
+            return back()->withErrors($e->getMessage());
+        }
+    }
+
+
+    public function remove(Request $request)
+    {
+        try{
+            $this->groupQuestion->id = $request->input('id');
+            $this->groupQuestion->procedure_id = $request->input('procedure_id');
+            $this->groupQuestion->remove($this->groupQuestion);
+            return back();
+        }catch (ValidationException $e){
+            return back()->withErrors($e->getMessage());
         }
     }
 }
--- app/Models/GroupQuestion.php
+++ app/Models/GroupQuestion.php
@@ -23,6 +23,17 @@ class GroupQuestion extends Model
         'procedure_id' => 'Procedimento'
     );
 
+    public static function change_priority($group_question_one, $group_question_two)
+    {
+        $group_question_one = GroupQuestion::findorFail($group_question_one->id);
+        $group_question_two = GroupQuestion::findorFail($group_question_two->id);
+        $aux =$group_question_one->priority;
+        $group_question_one->priority = $group_question_two->priority;
+        $group_question_two->priority = $aux;
+        $group_question_one->save();
+        $group_question_two->save();
+    }
+
     public function procedure(){
         return $this->belongsTo(Procedure::class);
     }
@@ -34,7 +45,7 @@ class GroupQuestion extends Model
     public function get_last_priority($procedure_id)
     {
         $groupQuestions = $this->read_by_procedure($procedure_id);
-        return $groupQuestions->lenth >0 ? $groupQuestions->last->priority+1: 1;
+        return $groupQuestions->count() > 0 ? $groupQuestions->last()->priority+1: 1;
     }
 
     public function create($object, $arguments = [])
@@ -48,7 +59,7 @@ class GroupQuestion extends Model
     {
         $groupQuestion_edit = GroupQuestion::findorFail($object->id);
         $groupQuestion_edit->title = $object->title;
-        if(ValidatorModel::validation($this->inputs($groupQuestion_edit),$this->rules($groupQuestion_edit->id),$this->attribute)){
+        if(ValidatorModel::validation($this->inputs($groupQuestion_edit),$this->rules(),$this->attribute)){
             return $groupQuestion_edit->save();
         }
     }
